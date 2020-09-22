ddf73a86a0ba8de4ec4926963335721dc0a21a39
Denis <alch007@gmail.com>
2020-09-22 21:06:47 +0300

    Initial commit

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int playerHealth; //объявили переменную с именем и типом
+        int enemyHealth;
+
+        //enemyHealth = playerHealth + 10 / 2;
+        //playerHealth = 50;
+
+        //print(enemyHealth);
+
+        //float damage = 4.5f;
+        //playerHealth = (int) damage;
+
+        //string playerName;
+        //playerName = "Denis";
+
+        //print("Hello " + playerName + ", your health: " + playerHealth);
+
+
+        //playerHealth = 0;
+
+        //bool isAlive;
+        //isAlive = playerHealth > 0 && enemyHealth > 0;
+
+
+        //isAlive = playerHealth > 0 || enemyHealth > 0;
+
+        //print("is player alive: " + isAlive);
+
+        bool isAlive = false;
+        playerHealth = -10;
+
+        if (playerHealth > 0)
+        {
+            //true
+            print("Player alive!");
+        }
+        else
+        {
+            //false
+            print("Player not alive :(");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // print("Update");
+    }
+}
---
+++ Assets/Scripts/NumbersTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumbersTest : MonoBehaviour
+{
+    //comment - CTRL + K + C
+    //uncomment - CTRL + K + U
+    //forma document - CTRL + K + D
+
+
+    int number;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        number = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //number = number + 1;
+        //number += 1; // -= 1
+        number++; //number--
+
+        print(number);
+    }
+}
